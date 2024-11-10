e6a64544dddfdfb76f940eff92be50513467c815
obulka <oleg.bulka@gmail,com>
2024-11-10 13:54:56 +0200

    Added no-cluster for 2 nodes

--- compose.sh
+++ compose.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 num_nodes=${1:-1}
 nodes_suffix=$(if [ "${num_nodes}" = "1" ]; then echo node; else echo nodes; fi)
+no_cluster=${no_cluster}
+if [ "$((no_cluster))" != "" ]; then
+  nodes_suffix="${nodes_suffix}-no-cluster"
+fi
 #echo ${nodes_suffix}
 #echo ${num_nodes}
 compose_file="docker-compose-${num_nodes}-${nodes_suffix}.yml"
